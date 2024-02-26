f9a1f56dcecf0889946226cfe914e62460085823
Asan <704000320asan@gmail.com>
2024-02-26 15:49:22 +0600

    created 3 classes (HelloName, SleepIn, StringTimes)

---
+++ src/main/java/org/PracticeTasks/AsanC/string1/HelloName.java
@@ -0,0 +1,10 @@
+package org.PracticeTasks.AsanC.warmup1.string1;
+
+public class HelloName {
+    public static void main(String[] args) {
+        System.out.println(helloName("Asan"));
+    }
+    public static String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+}

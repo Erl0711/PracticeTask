8bc2aaa600ec03c289141547e1bb91a397792c13
Erlan <erlanc312@gmail.com>
2024-02-28 16:52:54 +0600

    add 2 tasks

--- src/main/java/org/PracticeTasks/ErlanC/CodeWar8.java
+++ src/main/java/org/PracticeTasks/ErlanC/CodeWar8.java
@@ -2,4 +2,24 @@ package org.PracticeTasks.ErlanC;
 
 public class CodeWar8 {
 
+    /*
+    Согласно мифам о сотворении мира авраамических религий, Адам и Ева были первыми людьми, которые бродили по Земле.
+    Вы должны выполнять работу Бога. Метод creation должен возвращать массив длиной 2,
+    содержащий объекты (представляющие Адама и Еву).
+    Первый объект в массиве должен быть экземпляром класса Man. Второй должен быть экземпляром класса Woman.
+    Оба объекта должны быть подклассами Human. Ваша задача - реализовать классы Human, Man и Woman.
+     */
+    public static Human[] create(){
+        return new Human[] {new Man(),new Woman()};
+    }
+
+    public static class Human{
+
+    }
+    public static class Man extends Human{
+
+    }
+    public static class Woman extends Human{
+
+    }
 }

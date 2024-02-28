239f8596951efa4e47c95c6933b70261630d2c5b
Erlan <erlanc312@gmail.com>
2024-02-28 22:48:36 +0600

    add 1 ttask from C0deWars

---
+++ src/main/java/org/PracticeTasks/ErlanC/CodeWar10.java
@@ -0,0 +1,16 @@
+package org.PracticeTasks.ErlanC;
+
+public class CodeWar10 {
+
+    /*
+        Функция объединения строк
+    */
+
+    public static String combineNames(String first,String last) {
+        return first + " " + last;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(combineNames("Erlan","Chadaev"));
+    }
+}

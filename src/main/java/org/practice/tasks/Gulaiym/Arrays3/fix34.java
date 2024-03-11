1590aca7522efadd7cf998d991a06db2b7264262
Guli <gokombayeva@bk.ru>
2024-03-11 13:37:24 +0600

    Задачки Array3/Map2

--- src/main/java/org/practice/tasks/Gulaiym/Arrays3/fix34.java
+++ src/main/java/org/practice/tasks/Gulaiym/Arrays3/fix34.java
@@ -1,4 +1,44 @@
 package org.practice.tasks.Gulaiym.Arrays3;
+    /*    Возвращает массив, содержащий точно такие же числа,
+        что и заданный массив, но переставленный так,
+        чтобы за каждой тройкой сразу следовала четверка.
+        Не перемещайте тройки, но любое другое число может перемещаться.
+        Массив содержит одинаковое количество троек и четверок, после каждой тройки идет число,
+        отличное от тройки, а цифра 3 появляется в массиве раньше любой четверки.
+        fix34([1, 3, 1, 4]) → [1, 3, 4, 1]
+        fix34([1, 3, 1, 4, 4, 3, 1]) → [1, 3, 4, 1, 1, 3, 4]
+        fix34([3, 2, 2, 4]) → [3, 4, 2, 2]*/
+    public class fix34 {
+    public static void main(String[] args) {
+        int[] testArray = {1, 3, 1, 4};
+        System.out.println("Исходный массив:");
+        for (int num : testArray) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\nРезультат после вызова метода fix34:");
+        int[] resultArray = fix34(testArray);
+        for (int num : resultArray) {
+            System.out.print(num + " ");
+        }
+    }
+    public static int[] fix34(int[] nums) {
+            int i = 0;
+            while(i < nums.length && nums[i] != 3)
+                i++;
+            int j = i + 1;
+            while(j < nums.length && nums[j] != 4)
+                j++;
+            while(i < nums.length) {
+                if(nums[i] == 3) {
+                    int temp = nums[i+1];
+                    nums[i+1] = nums[j];
+                    nums[j] = temp;
+                    while(j < nums.length && nums[j] != 4)
+                        j++;
+                }
+                i++;
+            }
+            return nums;
+        }
+        }
 
-public class fix34 {
-}
--- src/main/java/org/practice/tasks/Gulaiym/Map2/word0.java
+++ src/main/java/org/practice/tasks/Gulaiym/Map2/word0.java
@@ -1,4 +1,29 @@
 package org.practice.tasks.Gulaiym.Map2;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/*  Учитывая массив строк, верните Map<String, Integer>,
+        содержащий ключ для каждой строки в массиве,
+        всегда со значением 0. Например, строка «hello» создает пару «hello»:0.
+        Позже мы проделаем более сложный подсчет, но в этой задаче значение просто равно 0.
+        word0(["a", "b", "a", "b"]) → {"a": 0, "b": 0}
+        word0(["a", "b", "a", "c", "b"]) → {"a": 0, "b": 0, "c": 0}
+        word0(["c", "b", "a"]) → {"a": 0, "b": 0, "c": 0}*/
 public class word0 {
-}
+    public static void main(String[] args) {
+                  String[] words= {"a", "b", "a", "c", "b"};
+                  Map<String, Integer> result = word0(words);
+                  System.out.println(result);
+              }
+              public static Map<String, Integer> word0(String[] words) {
+              Map<String, Integer> resultMap = new HashMap<>();
+              for (String word : words) {
+                  resultMap.put(word, 0);
+                  }
+                  return resultMap;
+              }
+          }
+
+
+

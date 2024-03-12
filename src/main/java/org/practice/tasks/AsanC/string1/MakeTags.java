cd515e36b27f0cfcf8becc09eaf8a7b60c999f34
Asan <704000320asan@gmail.com>
2024-03-12 06:02:39 +0600

    My issue in Jira(String1 makeTags)

---
+++ src/main/java/org/practice/tasks/AsanC/string1/MakeTags.java
@@ -0,0 +1,10 @@
+package org.practice.tasks.AsanC.string1;
+
+public class MakeTags {
+    public static void main(String[] args) {
+        System.out.println(makeTags("i","Yay"));
+    }
+    public static String makeTags(String tag, String word) {
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+}

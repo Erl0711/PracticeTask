6425395f07d109802837d6a0a27db67257b7e99f
Erlan <erlanc312@gmail.com>
2024-03-15 12:08:36 +0600

    add 2 tasks from CodingBat Array 3: linearIn, Series Up

---
+++ src/main/java/org/practice/tasks/ErlanC/coding_bat/LinearIn.java
@@ -0,0 +1,29 @@
+package org.practice.tasks.ErlanC.coding_bat;
+
+public class LinearIn {
+
+    /*
+    Учитывая два массива целых чисел, отсортированных в порядке возрастания, внешний и внутренний,
+     верните значение true, если все числа во внутреннем отображаются во внешнем.
+      Лучшее решение выполняет только один "линейный" проход по обоим массивам, используя тот факт,
+       что оба массива уже отсортированы по порядку.
+     */
+
+    public static void main(String[] args) {
+        System.out.println(linearIn(new int[]{1, 2, 3}, new int[]{}));
+    }
+
+    public static boolean linearIn(int[] outer, int[] inner) {
+        int innerInd = 0;
+        int count = 0;
+        for (int i = 0; i < outer.length; i++) {
+            if (inner.length != 0 && outer[i] == inner[innerInd]) {
+                count++;
+                innerInd++;
+            }
+            if (count == inner.length) return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/main/java/org/practice/tasks/ErlanC/coding_bat/SeriesUp.java
@@ -0,0 +1,23 @@
+package org.practice.tasks.ErlanC.coding_bat;
+
+import java.util.Arrays;
+
+public class SeriesUp {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(seriesUp(3)));
+    }
+    public static int[] seriesUp(int n) {
+        int[] arr = new int[n * (n + 1) / 2];
+        int val = 1;
+        for (int i = 0; i < arr.length; ) {
+            for (int j = 1; j <= val; j++) {
+                if (i < arr.length) {
+                    arr[i] = j;
+                    i++;
+                }
+            }
+            val++;
+        }
+        return arr;
+    }
+}

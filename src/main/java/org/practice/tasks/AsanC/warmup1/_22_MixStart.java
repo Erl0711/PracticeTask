362fdcf609e9d9874b3180a51ca24b169de120ac
Asan <704000320asan@gmail.com>
2024-03-21 09:50:27 +0600

    dobavil usloviya i camelCase

--- src/main/java/org/practice/tasks/AsanC/warmup1/_22_MixStart.java
+++ src/main/java/org/practice/tasks/AsanC/warmup1/_22_MixStart.java
@@ -1,6 +1,10 @@
 package org.practice.tasks.AsanC.warmup1;
 
 public class _22_MixStart {
+    /*
+    Return true if the given string begins with "mix",
+    except the 'm' can be anything, so "pix", "9ix" .. all count.
+     */
     public static boolean mixStart(String str) {
         return str.length() >= 3 && str.substring(1, 3).equals("ix");
     }
--- src/main/java/org/practice/tasks/AsanC/warmup1/_27_Max1020.java
+++ src/main/java/org/practice/tasks/AsanC/warmup1/_27_Max1020.java
@@ -7,8 +7,8 @@ public class _27_Max1020 {
     range 10..20 inclusive, or return 0 if neither is in that range.
      */
     public static int max1020(int a, int b) {
-        int resa = (a >= 10 && a <= 20) ? a : 0;
-        int resb = (b >= 10 && b <= 20) ? b : 0;
-        return Math.max(resa, resb);
+        int resA = (a >= 10 && a <= 20) ? a : 0;
+        int resB = (b >= 10 && b <= 20) ? b : 0;
+        return Math.max(resA, resB);
     }
 }

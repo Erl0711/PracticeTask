a1b1ea21a995e053db2862ec775bc55efee63b8d
Guli <gokombayeva@bk.ru>
2024-03-22 12:04:09 +0600

    String2/repeatEnd

--- src/main/java/org/practice/tasks/Gulaiym/String2/repeatEnd.java
+++ src/main/java/org/practice/tasks/Gulaiym/String2/repeatEnd.java
@@ -1,2 +1,24 @@
-package org.practice.tasks.Gulaiym.String2;public class repeatEnd {
-}
+package org.practice.tasks.Gulaiym.String2;
+/* Учитывая строку и целое число n,
+   верните строку, состоящую из n повторений последних n символов строки.
+    Вы можете предположить, что n находится в диапазоне от 0 до длины строки включительно.
+       repeatEnd("Hello", 3) → "llollollo"
+       repeatEnd("Hello", 2) → "lolo"
+       repeatEnd("Hello", 1) → "o"
+    */
+public class repeatEnd {
+    public static void main(String[] args) {
+// Примеры использования функции
+        System.out.println(repeatEnd("Hello", 3));
+        System.out.println(repeatEnd("Hello", 2));
+        System.out.println(repeatEnd("Hello", 1));
+    }
+    public static String repeatEnd(String str, int n) {
+        StringBuilder result = new StringBuilder();
+        String endSubstring = str.substring(str.length() - n);
+        for (int i = 0; i < n; i++) {
+            result.append(endSubstring);
+        }
+        return result.toString();
+    }
+    }

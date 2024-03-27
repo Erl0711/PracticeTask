01ae21e1b6811ed04f5eb7fca71a04f524ba21f0
Zhyldyz <jilduz0@gmail.com>
2024-03-27 10:59:38 +0600

    задача по сортировке по имени и возрасту

--- src/main/java/org/practice/tasks/zhyldyz/stream/MoreY.java
+++ src/main/java/org/practice/tasks/zhyldyz/stream/MoreY.java
@@ -5,9 +5,9 @@ import java.util.stream.Collectors;
 
 public class MoreY {
     //Given a list of strings, return a list where each string has "y" added at its start and end.
-    public static List <String> moreY(List<String> strings){
-        strings= strings.stream()
-                .map(n -> "y"+ n + "y")
+    public static List<String> moreY(List<String> strings) {
+        strings = strings.stream()
+                .map(n -> "y" + n + "y")
                 .collect(Collectors.toList());
         return strings;
     }

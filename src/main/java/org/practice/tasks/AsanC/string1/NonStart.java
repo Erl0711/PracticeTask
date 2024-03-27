d0d2762e97778aa490e0fc5d186b7439cb5cbf89
Asan <704000320asan@gmail.com>
2024-03-27 10:06:01 +0600

    Zabil Dobavit Uslovya zadachi

--- src/main/java/org/practice/tasks/AsanC/string1/NonStart.java
+++ src/main/java/org/practice/tasks/AsanC/string1/NonStart.java
@@ -1,6 +1,11 @@
 package org.practice.tasks.AsanC.string1;
 
 public class NonStart {
+    /*
+    Given 2 strings, return their concatenation,
+    except omit the first char of each.
+    The strings will be at least length 1.
+     */
     public static String nonStart(String a, String b) {
         return a.substring(1) + b.substring(1);
     }

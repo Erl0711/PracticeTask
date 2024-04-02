7b04ef1f3ce7a247bf4f3f1d2d84c50028963ee5
Nurj1git <nurzigit.0808@gmail.com>
2024-04-02 13:10:59 +0600

    Nurjigit codingB: String-3 > withoutString

--- src/main/java/org/practice/tasks/NurjigitC/CountYZ.java
+++ src/main/java/org/practice/tasks/NurjigitC/CountYZ.java
@@ -1,17 +1,14 @@
 package org.practice.tasks.NurjigitC;
 
 public class CountYZ {
-
-
-    public static int countYZ(String str) {
+    public  int countYZ(String str) {
         int count = 0;
         str = str.toLowerCase();
         if (str.endsWith("z") || str.endsWith("y")) {
             count++;
         }
         for (int i = 0; i < str.length() - 1; i++) {
-            if ((str.charAt(i) == 'z' || str.charAt(i) == 'y') &&
-                    !(Character.isLetter(str.charAt(i + 1)))) {
+            if ((str.charAt(i) == 'z' || str.charAt(i) == 'y') && !(Character.isLetter(str.charAt(i + 1)))) {
                 count++;
             }
         }
---
+++ src/main/java/org/practice/tasks/NurjigitC/main/WithoutStringMain.java
@@ -0,0 +1,10 @@
+package org.practice.tasks.NurjigitC.main;
+
+import org.practice.tasks.NurjigitC.WithoutString;
+
+public class WithoutStringMain {
+    public static void main(String[] args) {
+        WithoutString withoutString = new WithoutString();
+        System.out.println(withoutString.withoutString("Nurjigit","ig"));
+    }
+}

8ac40e3081a6d68be374dbb9b157d96208387501
Nurj1git <nurzigit.0808@gmail.com>
2024-04-02 11:38:35 +0600

    Nurjigit: countYZ v.2

--- src/main/java/org/practice/tasks/NurjigitC/CountYZ.java
+++ src/main/java/org/practice/tasks/NurjigitC/CountYZ.java
@@ -4,19 +4,19 @@ public class CountYZ {
     public static void main(String[] args) {
         System.out.println(countYZ("fez day"));
     }
+
     public static int countYZ(String str) {
         int count = 0;
-        str =  str.toLowerCase();
-        if(str.endsWith("z") || str.endsWith("y")){
+        str = str.toLowerCase();
+        if (str.endsWith("z") || str.endsWith("y")) {
             count++;
         }
-        for(int i = 0; i<str.length()-1; i++){
-            if((str.charAt(i) == 'z' || str.charAt(i) == 'y') &&
-                    !(Character.isLetter(str.charAt(i+1))) ){
+        for (int i = 0; i < str.length() - 1; i++) {
+            if ((str.charAt(i) == 'z' || str.charAt(i) == 'y') &&
+                    !(Character.isLetter(str.charAt(i + 1)))) {
                 count++;
             }
         }
         return count;
     }
-
 }

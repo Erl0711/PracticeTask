4d022e7a57f3cfa57ca78ed8ac50dbd852c183a0
Guli <gokombayeva@bk.ru>
2024-05-15 10:49:28 +0600

    test\Ally123

--- src/main/java/org/practice/tasks/Gulaiym/AllyTest.java
+++ src/main/java/org/practice/tasks/Gulaiym/AllyTest.java
@@ -1,2 +1,7 @@
-package org.practice.tasks.Gulaiym;public class AllyTest {
+package org.practice.tasks.Gulaiym;
+
+public class AllyTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Karakol!");
+    }
 }

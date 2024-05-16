075179c5631b9ca7919ec6d4a6a72ba85701ba65
Zhyldyz <jilduz0@gmail.com>
2024-05-16 10:44:48 +0600

    fix(AMMAPLE-2323)-добален коммент

--- src/main/java/org/practice/tasks/yrysbek/leetcode/PlusOne.java
+++ src/main/java/org/practice/tasks/yrysbek/leetcode/PlusOne.java
@@ -34,13 +34,16 @@ public class PlusOne extends BaseLeetCode {
      0 <= digits[i] <= 9
      digits does not contain any leading 0's.
      */
-
+    public static void main(String[] args) {
+        System.out.println("2");
+    }
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] + 1 != 10) {
                 digits[i] = digits[i] + 1;
                 return digits;
             } else {
+
                 digits[i] = 0;
             }
         }
@@ -50,6 +53,7 @@ public class PlusOne extends BaseLeetCode {
         } else {
             int[] newDigits = new int[digits.length + 1];
             newDigits[0] = 1;
+
             return newDigits;
         }
     }
